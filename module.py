aa2b722e63e94c490b0d0d08024466025eb9b44d
jaehyukjung <jhdool1594@gmail.com>
2023-01-01 21:59:55 +0900

    [23.01.01, 정재혁] 클래스 내부 수정

--- module.py
+++ module.py
@@ -9,23 +9,24 @@ class Prob_Instance:
         self.machine_list = [] # 기계 list
 
     def __repr__(self):
-        return str('Objective - ' + self.objective + ', Job - ' + str(self.job_list.__len__()) + ', Machine - ' + str(self.machine_list.__len__()))
+        return str(
+            'Objective - ' + self.objective + ', Job - ' + str(self.job_list.__len__()) + ', Machine - ' + str(
+                self.machine_list.__len__()) + '\n')
 
     def deepcopy(self):
         return copy.deepcopy(self)
 
 class Job: # 입력 데이터: job (요청)
-    def __init__(self, ID: int, Process_time, Due_date):
+    def __init__(self, ID: int, process_time, due_date):
         self.id = ID
-        self.process_time = Process_time
-        self.due_date = [0, Due_date]
+        self.process_time = process_time
+        self.due_date = due_date
         self.tardiness = 0
 
     def initialize(self):
         self.done = False
         self.priority = -1 # 우선순위
-        self.start_time = self.due_date[0] # 시작 시간
-
+        self.start_time = -1 # 시작 시간
 
 class Machine: # 작업 기계
     def __init__(self, ID: int):
@@ -41,6 +42,35 @@ class Machine: # 작업 기계
         self.can_work = True
         self.measures['makespan'] = 0
         self.measures['total_tardiness'] = 0
+
+    def work(self, target: Job):
+        if not self.doable(target): raise Exception('Infeasible Working!')
+        target.done = True
+        self.work_time = target.process_time / self.speed
+        target.start_time = self.avail_time
+        self.avail_time += self.work_time
+        target.tardiness = max(0, self.avail_time - target.due_date)
+        self.measures['total_tardiness'] += target.tardiness
+        self.measures['makespan'] = self.avail_time
+        self.served_job.append(target.id)
+
+
+    def doable(self, target: Job) -> bool: # -> return 값 힌트
+        if target.done:
+            return False
+        else:
+            return True
+
+    def __repr__(self):
+        return str('Machine # ' + str(self.id))
+
+class Setup_Machine(Machine): # 작업 기계
+    def __init__(self, ID: int, SETUP: str):
+        Machine.__init__(self,ID)
+        self.Setup = SETUP
+
+    def initialize(self):
+        Machine.initialize(self)
 
     def work(self, target: Job):
         if not self.doable(target): raise Exception('Infeasible Working!')
