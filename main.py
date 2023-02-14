174281771a02d6377fa76c79b7259ad647b75bbb
jaehyukjung <jhdool1594@gmail.com>
2023-02-14 22:01:03 +0900

    [23.02,14 정재혁]

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from mutation_solver import *
 from crossover_solver import *
 from gantt_chart import *
 
-def random_LoadProb(id:int) -> Prob_Instance:
+
+def random_LoadProb(id: int) -> Prob_Instance:
     random.seed(100)
     random_job = 15
     random_machine = 5
@@ -14,7 +15,7 @@ def random_LoadProb(id:int) -> Prob_Instance:
     ThisProb.chromo = Chromosome(id)
     for i in range(random_job):
         ThisProb.job_list.append(Job(i + 1, random.randint(2, 8), random.randint(6, 14), random.randint(1, 3),
-                                            random.randint(0, 2), random.choice(STATUSLIST)))
+                                     random.randint(0, 2), random.choice(STATUSLIST)))
     for i in range(random_machine):
         ThisProb.machine_list.append(Machine(i + 1, random.choice(STATUSLIST)))
 
@@ -23,6 +24,7 @@ def random_LoadProb(id:int) -> Prob_Instance:
     set_work_speed_matrix(ThisProb.job_list, ThisProb.machine_list)
 
     return ThisProb
+
 
 if __name__ == "__main__":
     population = []
@@ -34,31 +36,28 @@ if __name__ == "__main__":
         print('Sum of Comepletion Time is ' + str(Solution['Objective']))
     population = sorted(population, key=lambda x: x[0].objective)[:100]
 
-    for i in range(10): # generation = 10
-        population2 = []
-        for i in range(501, 601): # mutate population 생성
+    for j in range(10):  # generation = 10
+
+        for i in range(100):  # mutate population 생성
             Sample = random_LoadProb(i)
             mach_setup = [[i.setup_status] for i in Sample.machine_list]
-            Solution, chromo,mach_list, sch_list = mut_solver(Sample, i, population[i - 501][0])
-            population2.append((chromo, Solution, sch_list))
+            Solution, chromo, mach_list, sch_list = mut_solver(Sample, i, population[i][0])
+            population.append((chromo, Solution, sch_list))
             print('Sum of Comepletion Time is ' + str(Solution['Objective']))
 
-        population3 = []
-        for i in range(601, 700): # crossover population 생성
+        for i in range(100):  # crossover population 생성
             Sample = random_LoadProb(i)
             mach_setup = [[i.setup_status] for i in Sample.machine_list]
-            Solution, chromo, mach_list, sch_list = cross_solver(Sample, i, population[i - 601][0], population[i - 600][0])
-            population3.append((chromo, Solution, sch_list))
+            Solution, chromo, mach_list, sch_list = cross_solver(Sample, i, population[i][0], population[i + 1][0])
+            population.append((chromo, Solution, sch_list))
             print('Sum of Comepletion Time is ' + str(Solution['Objective']))
 
-        population4 = []
-        for i in range(700, 800): # random population 생성
+        for i in range(100):  # random population 생성
             Sample = random_LoadProb(i)
             mach_setup = [[i.setup_status] for i in Sample.machine_list]
-            Solution, chromo, mach_list, sch_list = rule_solver(Sample, i)
-            population4.append((chromo, Solution,sch_list))
+            Solution, chromo, mach_list, sch_list = rule_solver(Sample, j * 100 + i)
+            population.append((chromo, Solution, sch_list))
             print('Sum of Comepletion Time is ' + str(Solution['Objective']))
 
-        population5 = population + population2 + population3 + population4
-        population = sorted(population5, key=lambda x: x[0].objective)[:100]
-        ganttChart(population[0][2], population[0][1], mach_setup) # 제일 좋은 결과값 간트 차트
+        population = sorted(population, key=lambda x: x[0].objective)[:100]
+        ganttChart(population[0][2], population[0][1], mach_setup)  # 제일 좋은 결과값 간트 차트
